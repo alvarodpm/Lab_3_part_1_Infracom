e87507a573f8a0de21f8a3b109ee6792890cb78c
Alvaro Plata <ad.plata@uniandes.edu.co>
2022-03-20 11:14:55 -0500

    Update client.py

--- client.py
+++ client.py
@@ -6,21 +6,27 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port on the server given by the caller
 server_address = (sys.argv[1], 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 try:
+
+    with open("file2", "rb") as f:
+        message = f.read()
     
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+        #message = 'This is the message.  It will be repeated.'.encode()
+        #print('sending "%s"' % message)
+        print('sending message')
 
-    amount_received = 0
-    amount_expected = len(message)
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
-
+        print("finished sending")
+        sock.sendall(message)
+    
+        amount_received = 0
+        amount_expected = len(message)
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            #print('received "%s"' % data)
+        print("received everything")
 finally:
     sock.close()
