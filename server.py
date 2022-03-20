19799ccf504945fa69bdc8da6ec6daf3fb7e36dc
Alvaro Plata <ad.plata@uniandes.edu.co>
2022-03-20 11:15:14 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+from _thread import *
+import os
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,21 +9,38 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the address given on the command line
 server_name = sys.argv[1]
 server_address = (server_name, 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 sock.listen(1)
 
-while True:
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+ThreadCount = 0
+
+def threaded_client(connection, ThreadCount):
+    connection.send(str.encode('Welcome to the Server'))
     try:
-        print >>sys.stderr, 'client connected:', client_address
+        print('client connected:', client_address)
         while True:
             data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
+            #print('received "%s"' % data)
             if data:
                 connection.sendall(data)
             else:
                 break
-    finally:
+        print("sent everything to", client_address)
+    except:
+        ThreadCount -= 1
         connection.close()
+    finally:
+        ThreadCount -= 1
+        connection.close()
+
+while True:
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    
+    print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
+    start_new_thread(threaded_client, (connection, ThreadCount))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+    
+sock.close()
