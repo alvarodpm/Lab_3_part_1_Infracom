edb5d85e5f181f2f05757e6ec71c67f7adb29893
Alvaro Plata <ad.plata@uniandes.edu.co>
2022-03-20 20:09:25 -0500

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -1,10 +1,12 @@
 import os
 from threading import Thread
 
+NumClientes = int(input('Seleccione el número de clientes que a los que desea enviar el archivo. Máximo 24: ').rstrip())
+
 def task():
     os.system('python client.py 127.0.0.1')
 
-for i in range(5):
+for i in range(NumClientes): 
 	print("new thread")
 	new_thread = Thread(target=task)
 	new_thread.start()
